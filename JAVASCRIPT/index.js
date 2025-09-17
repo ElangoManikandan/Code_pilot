0d20830093120a35246c716de4a43e164277673a
elangomanikandan <elangomanikandan31204@gmail.com>
2025-09-17 09:37:51 +0530

    17-09-25 10 AM

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -11,3 +11,14 @@ function func(){
 }
 func()
 // console.log(i)
+console.log(typeof 10);
+console.log(typeof 2.4);
+console.log(typeof func);
+console.log(typeof null);
+console.log(typeof "Hello");
+console.log(typeof [1,2,"qwe"]);
+console.log(typeof {number:123,"name":"asd"});
+// console.log(typeof null);
+let x=5;
+console.log();
+
