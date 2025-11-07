b7dd3c2e0afef47f455b08f0bc757cf20694f7eb
elangomanikandan <elangomanikandan31204@gmail.com>
2025-11-08 00:04:17 +0530

    Form

---
+++ REACT/my-react-app/src/pages/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return <div>
+        <nav style={{display:"flex",gap:"30px",alignItems:"flex-end"}}>
+            <a href="">Home</a>
+            <a href="">Products</a>
+            <a href="">About us</a>
+            <a href="">Contact</a>
+        </nav>
+    </div>
+}
